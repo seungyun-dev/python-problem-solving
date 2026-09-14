37f0e0a7556e3099751fb5ce35509250706f17af
seungyun-dev <ksy120529@gmail.com>
2026-09-14 10:47:46 +0900

    Update solution.py

--- problems/binary-search/보석나누기-문제/solution.py
+++ problems/binary-search/보석나누기-문제/solution.py
@@ -10,7 +10,7 @@ r = 0
 while start <= end: # 이진 탐색
     mid = (start+end)//2
     
-    p = 0 # 
+    p = 0 # 보석을 mid개씩 나누었을 때에 최대 몇명에게 나누어 줄 수 있는지
     for i in k:
         p += i//mid 
         if i % mid > 0: # 헷갈렸던 부분
